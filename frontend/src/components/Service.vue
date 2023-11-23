9d83baa64fb4255abe22eb74896921e3d4cdd667
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-11-23 18:55:01 +0100

    service card created and styled

--- frontend/src/components/Service.vue
+++ frontend/src/components/Service.vue
@@ -3,16 +3,22 @@
     <div v-for="service in services" :key="service.id">
       <router-link :to="service.path">
         <div
-          class="flex justify-between items-center border px-3 py-2 mb-2 rounded"
+          class="flex justify-center items-center border px-3 py-3 mb-2 rounded bg-slate-100 service-card"
         >
-          <div class="w-[20vw]">
-            <img src="" alt="" />
+          <div class="w-[40vw]">
+            <img
+              :src="service.image"
+              alt="haarImage"
+              class="w-[60px] h-[60px] rounded-full border-slate-500 ]"
+            />
           </div>
-          <div class="flex flex-col">
-            <h3 class="text-[17px] w-[30vw]">{{ service.name }}</h3>
+          <div class="flex flex-col gap-1 ml-4">
+            <h3 class="text-[17px] w-[36vw]">{{ service.name }}</h3>
             <p class="text-[12px]">{{ service.price }}</p>
           </div>
-          <div class="w-[15vw]">{{ service.icon }}</div>
+          <div class="w-[15vw]">
+            <right-arrow />
+          </div>
         </div>
       </router-link>
     </div>
@@ -20,6 +26,7 @@
 </template>
 
 <script setup lang="ts">
+import RightArrow from '../assets/icons/rightArrow.vue';
 import { useServiceStore } from '../stores/serviceStore';
 
 const serviceStore = useServiceStore();
