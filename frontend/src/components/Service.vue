6ec5176e65fba3826fc20af1b83df624a73779dc
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-11-23 11:40:36 +0100

    created navigation to calendar

--- frontend/src/components/Service.vue
+++ frontend/src/components/Service.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="w-[50vw]">
-    <div
-      v-for="service in services"
-      :key="service.id"
-      class="flex justify-between items-center border px-3 py-2 mb-2 rounded"
-    >
-      <div class="w-[20vw]">
-        <img src="" alt="" />
-      </div>
-      <div class="flex flex-col">
-        <h3 class="text-[17px] w-[30vw]">{{ service.name }}</h3>
-        <p class="text-[12px]">{{ service.price }}</p>
-      </div>
-      <div class="w-[15vw]">{{ service.icon }}</div>
+    <div v-for="service in services" :key="service.id">
+      <router-link :to="service.path">
+        <div
+          class="flex justify-between items-center border px-3 py-2 mb-2 rounded"
+        >
+          <div class="w-[20vw]">
+            <img src="" alt="" />
+          </div>
+          <div class="flex flex-col">
+            <h3 class="text-[17px] w-[30vw]">{{ service.name }}</h3>
+            <p class="text-[12px]">{{ service.price }}</p>
+          </div>
+          <div class="w-[15vw]">{{ service.icon }}</div>
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
